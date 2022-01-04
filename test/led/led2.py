991a2e1a21333019bd05dfeddabc22d6716abba2
Adam Blomfield <adam@blomfield.me>
2022-01-04 15:40:44 +0000

    test

--- test/led/led2.py
+++ test/led/led2.py
@@ -14,20 +14,55 @@ pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
 )
 
-
-def rainbow_cycle(wait):
-    for j in range(255):
-            pixel_index = (i * 256 // num_pixels) + j
-
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
 
 # Red Ring
-loopcount = range(10)
+loopcount = range(3)
 pixring = range(1,num_pixels)
 for n in loopcount:
     for i in pixring:
         pixels[i] = (255, 0, 0)
         pixels.show()
-        time.sleep(0.05)
+        time.sleep(0.075)
+        pixels[i] = (0, 0, 0)
+
+# Green Ring
+loopcount = range(3)
+pixring = range(1,num_pixels)
+for n in loopcount:
+    for i in pixring:
+        pixels[i] = (0, 255, 0)
+        pixels.show()
+        time.sleep(0.075)
+        pixels[i] = (0, 0, 0)
+
+# Blue Ring
+loopcount = range(3)
+pixring = range(1,num_pixels)
+for n in loopcount:
+    for i in pixring:
+        pixels[i] = (0, 0, 255)
+        pixels.show()
+        time.sleep(0.075)
         pixels[i] = (0, 0, 0)
 
 # Pixels Off
